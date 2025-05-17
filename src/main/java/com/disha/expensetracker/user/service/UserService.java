3f645f367b3874763630465f2e0d432d22d1aa9e
Disha S K <dishask@Dishas-MacBook-Air.local>
2025-05-17 18:09:10 +0530

    getting user information details using jwt token authorization

--- src/main/java/com/disha/expensetracker/user/service/UserService.java
+++ src/main/java/com/disha/expensetracker/user/service/UserService.java
@@ -1,26 +1,38 @@
 package com.disha.expensetracker.user.service;
 
+import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.disha.expensetracker.user.data.UserResponse;
 import com.disha.expensetracker.user.domain.CustomUserDetails;
 import com.disha.expensetracker.user.domain.User;
-import com.disha.expensetracker.user.domain.UserDetailsRepository;
+import com.disha.expensetracker.user.domain.UserDetailsRepositoryWrapper;
 
 @Service
 public class UserService implements UserDetailsService {
 
     @Autowired
-    private UserDetailsRepository userRepository;
+    private UserDetailsRepositoryWrapper userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-        User user = userRepository.findByUserName(userName)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-        return new CustomUserDetails(user);
+        try {
+            User user = userRepository.findByUserName(userName);
+            return new CustomUserDetails(user);
+        } catch (BadRequestException ex) {
+            return null;
+        }
+    }
+
+    public UserResponse getCurrentUserDetails(Authentication authentication) throws BadRequestException {
+        String username = authentication.getName();
+        User user = userRepository.findByUserName(username);
+        return UserResponse.builder().email(user.getEmail()).userName(username).isActive(user.getIsActive()).build();
     }
 
 }
